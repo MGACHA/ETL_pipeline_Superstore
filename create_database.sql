f053b2f8b8ac2b2976c5a028788dc38b03de9ad6
MGACHA <71405661+MGACHA@users.noreply.github.com>
2026-04-14 15:00:26 +0100

    Create initial database setup for etl_project

---
+++ create_database.sql
@@ -0,0 +1,19 @@
+create database etl_project
+
+go
+
+use etl_project
+
+go
+
+
+/*-- DROP database
+USE master;
+GO
+
+ALTER DATABASE etl_project SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+GO
+
+DROP DATABASE etl_project;
+GO
+*/
